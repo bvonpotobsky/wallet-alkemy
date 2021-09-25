9b6e3bd893c48679fdab6e44777af0e9927f3129
Benjamín von Potobsky <bvonpotobsky@gmail.com>
2021-09-25 04:40:15 -0400

    :rocket:  - Ready to deploy
    App is ready to deploy, but the EditModal to update transactions still needs to be fixed'

--- index.js
+++ index.js
@@ -2,13 +2,20 @@ const express = require("express");
 const cors = require("cors");
 const pool = require("./db");
 
+const path = require("path");
+const PORT = process.env.PORT || 5000;
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.listen(5000, () => {
-  console.log("THE SERVER HAS STARTED");
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
+}
+
+app.listen(PORT, () => {
+  console.log(`THE SERVER HAS STARTED ON PORT ${PORT}`);
 });
 
 // ROUTES
