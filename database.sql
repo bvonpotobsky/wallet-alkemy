e86f3fdae0e2e5370be8c69800480fb8fdb8e72d
Benjamín von Potobsky <bvonpotobsky@gmail.com>
2021-09-25 03:27:46 -0400

    Rename file

--- database.sql
+++ database.sql
@@ -2,7 +2,7 @@ CREATE DATABASE wallet;
 
 CREATE TABLE transactions(
   id SERIAL PRIMARY KEY,
-  text VARCHAR(255),
-  amount NUMERIC(15, 9),
+  text VARCHAR(255) NOT NULL,
+  amount NUMERIC(11, 2) NOT NULL,
   date DATE
 );
