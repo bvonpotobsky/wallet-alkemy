f48030da78581f30578d451c8fffaf29d22d1a7f
Benjamín von Potobsky <bvonpotobsky@gmail.com>
2021-12-10 16:54:48 -0300

    cors

--- app.js
+++ app.js
@@ -3,26 +3,27 @@ const cors = require("cors");
 const routerAPI = require("./routes/");
 
 const app = express();
-app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-const whitelist = [
-  "https://wallet-alkemy.vercel.app/",
-  "http://localhost:3001",
-];
-const options = {
-  origin: (origin, callback) => {
-    if (whitelist.includes(origin) || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error("no permitido"));
-    }
-  },
-};
-app.use(cors(options));
+// const whitelist = [
+//   "https://wallet-alkemy.vercel.app/",
+//   "http://localhost:3001",
+// ];
+// const options = {
+//   origin: (origin, callback) => {
+//     if (whitelist.includes(origin) || !origin) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error("no permitido"));
+//     }
+//   },
+// };
+
+app.use(cors());
+app.use(express.json());
 
 routerAPI(app);
 
